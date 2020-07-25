7f844c1a2b30fe38ba09b61aa5d1b8b75f85f7dc
Rudr Thakur <rudrakshacmkt777@gmail.com>
2020-07-25 11:49:24 +0530

    search and filter tickets

--- gss/app/Http/Controllers/User/TicketController.php
+++ gss/app/Http/Controllers/User/TicketController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\ServiceAction;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -13,12 +14,14 @@ use App\Ticket;
 class TicketController extends Controller
 {
 
-    public function __construct(){
+    public function __construct()
+    {
 
         $this->middleware('auth');
     }
 
-    public function all(){
+    public function all()
+    {
 
         $tickets = new Ticket;
 
@@ -29,75 +32,91 @@ class TicketController extends Controller
             'status_id',
         ];
 
-        foreach($columns as $column){
-            if(request()->has($column)){
+        foreach ($columns as $column) {
+            if (request()->has($column)) {
                 $tickets = $tickets->where($column, request($column));
                 $queries[$column] = request($column);
             }
         }
 
-         $tickets =  $tickets->with('authority')
-                            ->with('user')
-                            ->with('status')
-                            ->paginate(10)
-                            ->appends($queries);
+        if (request()->has('user_name') && request('user_name') != '') {
+
+            $userIds = User::where('name', 'like', '%' . request('user_name') . '%')->get();
+
+            $tickets = $tickets->whereIn('user_id', $userIds);
+        }
+
+        if(request()->has('user_id')){
+
+            $tickets = $tickets->where('user_id', request('user_id'));
+
+        }
+
+        $tickets = $tickets->with('authority')
+            ->with('user')
+            ->with('status')
+            ->paginate(10)
+            ->appends($queries);
 
         return view('user.tickets', ['tickets' => $tickets]);
 
     }
 
-    public function index($ticketId){
-         $ticket = Ticket::with('authority')
-                                    ->with('user')
-                                    ->with('status')
-                                    ->with('type')
-                                    ->where('id', $ticketId)
-                                    ->firstOrFail();
+    public function index($ticketId)
+    {
+        $ticket = Ticket::with('authority')
+            ->with('user')
+            ->with('status')
+            ->with('type')
+            ->where('id', $ticketId)
+            ->firstOrFail();
 
         return view('user.ticket-details', ['ticket' => $ticket]);
 
     }
 
-    public static function detail($ticketId){
-
-         $ticket = Ticket::with('authority')
-                                    ->with('user')
-                                    ->with('status')
-                                    ->with('type')
-                                    ->where('id', $ticketId)
-                                    ->firstOrFail();
-
-        $service = Service::with('priority')
-                                    ->where('ticket_id', $ticketId)->firstOrFail();
-
-        return ['ticket' => $ticket,
-                'service' => $service];
-    }
-
-    public static function show($ticketId){
+    public static function detail($ticketId)
+    {
 
         $ticket = Ticket::with('authority')
-                                    ->with('user')
-                                    ->with('status')
-                                    ->where('id', $ticketId)
-                                    ->firstOrFail();
+            ->with('user')
+            ->with('status')
+            ->with('type')
+            ->where('id', $ticketId)
+            ->firstOrFail();
 
         $service = Service::with('priority')
-                                    ->where('ticket_id', $ticketId)->firstOrFail();
+            ->where('ticket_id', $ticketId)->firstOrFail();
+
+        return ['ticket' => $ticket,
+            'service' => $service];
+    }
+
+    public static function show($ticketId)
+    {
+
+        $ticket = Ticket::with('authority')
+            ->with('user')
+            ->with('status')
+            ->where('id', $ticketId)
+            ->firstOrFail();
+
+        $service = Service::with('priority')
+            ->where('ticket_id', $ticketId)->firstOrFail();
 
         $serviceAction = ServiceAction::with('worker')
-                                                ->where('service_id', $service->id)
-                                                ->firstOrFail();
+            ->where('service_id', $service->id)
+            ->firstOrFail();
 
         return [
 
-                'service' => $service,
+            'service' => $service,
 
-                'ticket' => $ticket,
+            'ticket' => $ticket,
 
-                'serviceAction' => $serviceAction,
+            'serviceAction' => $serviceAction,
 
-                ];
+        ];
 
     }
 }
--- gss/resources/views/user/tickets.blade.php
+++ gss/resources/views/user/tickets.blade.php
@@ -4,129 +4,92 @@
 
 @section('content')
 
-<!-- Begin Page Content -->
-<div class="container-fluid">
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
-  <!-- Page Heading -->
-  <h1 class="h3 mb-2 text-gray-800">Tickets</h1>
-  <p class="mb-4">These are the tickets that were raised by you</p>
+        <!-- Page Heading -->
+        <h1 class="h3 mb-2 text-gray-800">Tickets</h1>
+        <p class="mb-4">These are the tickets that were raised by you</p>
 
-  <!-- DataTales Example -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Tickets Info</h6>
-    </div>
-    <div class="card-body">
         <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Filters</h6>
+            <div class="card-header py-3">
+                <div class="btn-group float-right">
+                    <a href="#" data-toggle="modal"
+                       data-target="#ticket-search-modal"><h6
+                            class="m-2 font-weight-bold text-primary">Search | </h6></a>
+                    <a href="#" data-toggle="modal"
+                       data-target="#ticket-filter-modal"><h6
+                            class="m-2 font-weight-bold text-primary">Filters | </h6></a>
+                    <a href="/tickets"><h6
+                            class="m-2 font-weight-bold text-primary">Clear Filters</h6></a>
                 </div>
+            </div>
             <div class="card-body">
-                <form method="GET" action="/tickets">
-                        <h6>Ticket Type</h6>
-                    <hr>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="services" value="1" name="type_id[]">
-                      <label class="form-check-label" for="services">Services</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="consumables" value="2" name="type_id[]">
-                      <label class="form-check-label" for="consumables">Consumables</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="capitalequipments" value="3" name="type_id[]">
-                      <label class="form-check-label" for="capitalequipments">Capital Equipments</label>
-                    </div>
-                     <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="hallbookings" value="4" name="type_id[]">
-                      <label class="form-check-label" for="hallbookings">Hall Bookings</label>
-                    </div>
-                    <hr>
-                          <h6>Ticket Status</h6>
-                    <hr>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="pending" value="1" name="status_id[]">
-                      <label class="form-check-label" for="pending">Pending</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="inprogress" value="2" name="status_id[]">
-                      <label class="form-check-label" for="inprogress">In Progress</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="onhold" value="3" name="status_id[]">
-                      <label class="form-check-label" for="onhold">On Hold</label>
-                    </div>
-                     <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" id="closed" value="4" name="status_id[]">
-                      <label class="form-check-label" for="closed">Closed</label>
-                    </div>
-                    <hr>
-                    <button class="btn btn-success" type="submit">Apply</button>
-                    <a href="/tickets" class="btn btn-primary" type="submit">Clear</a>
-                </form>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>Ticket Id</th>
+                            <th>Raised By</th>
+                            <th>Type</th>
+                            <th>Current Holder</th>
+                            <th>Status</th>
+                            <th>Raised On</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th>Ticket Id</th>
+                            <th>Raised By</th>
+                            <th>Type</th>
+                            <th>Current Holder</th>
+                            <th>Status</th>
+                            <th>Raised On</th>
+                            <th>Action</th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
 
+                        @foreach($tickets as $ticket)
+                            <tr>
+                                <td>{{ $ticket->id }}</td>
+                                <td>{{ $ticket->user->name }}</td>
+                                <td>{{ $ticket->type->type }}</td>
+                                <td>{{ $ticket->authority->name }}</td>
+                                <td>{{ $ticket->status->status }}</td>
+                                <td>{{ $ticket->created_at }}</td>
+                                <td><a href="javascript:void(0)" id="{{ $ticket->id }}"
+                                       class="service-show btn btn-outline-primary btn-sm"><i
+                                            class="fas fa-eye"></i></a>
+                                    <a href="javascript:void(0)" id="{{ $ticket->id }}"
+                                       class="service-action btn btn-outline-primary btn-sm"><i
+                                            class="fas fa-tasks"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        {{ $tickets->links() }}
+                    </div>
+                </div>
 
             </div>
         </div>
-      <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>Ticket Id</th>
-              <th>Type</th>
-              <th>Current Holder</th>
-              <th>Status</th>
-              <th>Created At</th>
-              <th>Updated At</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th>Ticket Id</th>
-              <th>Type</th>
-              <th>Current Holder</th>
-              <th>Status</th>
-              <th>Created At</th>
-              <th>Updated At</th>
-              <th>Action</th>
-            </tr>
-          </tfoot>
-          <tbody>
 
-            @foreach($tickets as $ticket)
-            <tr>
-              <td>{{ $ticket->id }}</td>
-              <td>{{ $ticket->type->type }}</td>
-              <td>{{ $ticket->authority->name }}</td>
-              <td>{{ $ticket->status->status }}</td>
-              <td>{{ $ticket->created_at }}</td>
-              <td>{{ $ticket->updated_at }}</td>
-              <td><a href="javascript:void(0)" id="{{ $ticket->id }}"
-                  class="service-show btn btn-outline-primary btn-sm"><i class="fas fa-eye"></i></a>
-                <a href="javascript:void(0)" id="{{ $ticket->id }}"
-                  class="service-action btn btn-outline-primary btn-sm"><i class="fas fa-tasks"></i></a>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
+        @include('user.partials.modals.ticket-filter-modal')
 
-        </table>
-      </div>
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center">
-                {{ $tickets->links() }}
-            </div>
-        </div>
+        @include('user.partials.modals.ticket-search-modal')
+
+        @include('user.partials.modals.service-details-modal')
+
+        @include('user.partials.modals.service-action-modal')
 
     </div>
-  </div>
-
-  @include('user.partials.modals.service-details-modal')
-
-  @include('user.partials.modals.service-action-modal')
-
-</div>
-<!-- /.container-fluid -->
+    <!-- /.container-fluid -->
 
 @endsection
