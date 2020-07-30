06f74bfbf4ba00b8b2ae228f35d05c9abebc872f
Rudr Thakur <rudrakshacmkt777@gmail.com>
2020-07-30 16:49:06 +0530

    bug fixes

--- gss/resources/views/user/create-permission.blade.php
+++ gss/resources/views/user/create-permission.blade.php
@@ -10,7 +10,7 @@
             <div class="col-lg-5 col-xl-5 col-md-12 col-12">
                 <h6 class="text-center">Create Permission</h6>
                 <hr>
-                @include('user.partials.forms.create-permission-form');
+                @include('user.partials.forms.create-permission-form')
             </div>
         </div>
     </div>
--- gss/resources/views/user/partials/forms/create-permission-form.blade.php
+++ gss/resources/views/user/partials/forms/create-permission-form.blade.php
@@ -3,7 +3,7 @@
     <div class="form-group">
         <lable for="role-name">Enter Permission</lable>
         <input type="text" class="form-control" name="permission_name" id="permission-name"
-               aria-describedby="role-name" placeholder="Enter Permission Name">
+               aria-describedby="role-name" placeholder="Enter Permission Name" required>
     </div>
     <hr>
     <button class="btn btn-primary" type="submit">Create</button>
